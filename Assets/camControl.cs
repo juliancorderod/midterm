02d839cbac80653a5ebdc170662f9927751e0e40
Unknown <jc6984@nyu.edu>
2017-02-21 13:52:50 -0500

    added camlift, camframe

--- Assets/camControl.cs
+++ Assets/camControl.cs
@@ -8,8 +8,17 @@ public class camControl : MonoBehaviour {
 
 	public bool endPic = false;
 	public bool takenPic = false;
+	public bool takingPic = false;
+
+	public bool liftCamTrue = false;
 
 	public int shutterSpeed;
+
+	public GameObject cameraMesh;
+	public GameObject camFrame;
+
+	float camMovLerp = 0f;
+
 
 
 
@@ -17,24 +26,57 @@ public class camControl : MonoBehaviour {
 	void Start () {
 		cam = GetComponent<Camera>();
 
+
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-		if(Input.GetKeyDown(KeyCode.Space)){
 
-			cam.clearFlags = CameraClearFlags.Nothing;
+
+//		cameraMesh.transform.localPosition = new Vector3(Mathf.Lerp(cameraMesh.transform.localPosition.x, 0,camMovLerp), 
+//			Mathf.Lerp(cameraMesh.transform.localPosition.y, 0.5f,camMovLerp),
+//			Mathf.Lerp(cameraMesh.transform.localPosition.z, 0.5f,camMovLerp));
+
+		cameraMesh.transform.localPosition = new Vector3(Mathf.Lerp(0.11f, 0,camMovLerp), Mathf.Lerp( 0.19f, 0.5f,camMovLerp), Mathf.Lerp(0.82f, 0.5f,camMovLerp));
+
+		if(Input.GetKeyDown(KeyCode.Tab) && !takingPic){
+
+			//camMovLerp += 1f * Time.deltaTime;
+
+			liftCamTrue = true; 
+		}
+		if (liftCamTrue && camMovLerp <= 1){
+
+			StartCoroutine (liftCam());
+
+		}
+		if(Input.GetKeyDown(KeyCode.Tab) && takingPic){
+			
+			liftCamTrue = false;
+		}
+		if (!liftCamTrue && camMovLerp >= 0){
+
+			StartCoroutine (camBack());
+
+		}
+
+//		if(!Input.GetKey(KeyCode.Tab) && !takingPic){
+//
+//			camMovLerp -= 0.5f * Time.deltaTime;
+//
+//		}
+
+
+		if(Input.GetKeyDown(KeyCode.Space) && takingPic){
+
 
 			StartCoroutine (startPic());
 
+
 		} 
-		//if(Input.GetKeyUp(KeyCode.Space)){
-//		if(endPic){
-//
-//			Application.CaptureScreenshot( "Assets/Resources/pic 1.png");
-//			takenPic = true;
-//		}
+
 
 
 		if (takenPic) {
@@ -42,10 +84,53 @@ public class camControl : MonoBehaviour {
 			cam.clearFlags = CameraClearFlags.SolidColor;
 			takenPic = false;
 		}
-		
+
+		Debug.Log(camMovLerp);
+
+
+
+
 	}
 
+	public IEnumerator liftCam(){
+
+		camMovLerp += 1f * Time.deltaTime;
+
+		while (camMovLerp <= 1f){
+
+			yield return null;
+		}
+			
+
+		cameraMesh.SetActive(false);
+
+		takingPic = true;
+
+		camFrame.SetActive(true);
+	}
+
+	public IEnumerator camBack(){
+
+		camFrame.SetActive(false);
+
+		camMovLerp -= 1f * Time.deltaTime;
+
+		while (camMovLerp <= 0.8f){
+
+			yield return null;
+		}
+
+		cameraMesh.SetActive(true);
+
+		takingPic = false;
+
+	}
+
+
 	public IEnumerator startPic(){
+
+
+		cam.clearFlags = CameraClearFlags.Nothing;
 
 		yield return new WaitForSeconds(shutterSpeed);
 
